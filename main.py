957c458ca0af1538d78bfffe6abfa0d7696a48e0
Kevin James Baxter <meta.data@verizon.net>
2026-07-17 00:17:24 -0400

    Added external LED.

--- main.py
+++ main.py
@@ -2,29 +2,56 @@
 
 from machine import Pin
 from utime import sleep
+import uasyncio
+
 
 onboard_led = Pin("PA5", Pin.OUT)
-
+onboard_led .off()
+pc10_led = Pin("PC10", Pin.OUT)
+pc10_led.off()
 
 ###############################################################################
 ################################### blync LED #################################
 ###############################################################################
-def blync(delay):
+async def blync(delay):
     while True:
         try:
             onboard_led.value(not onboard_led.value())
-            sleep(delay) # sleep 1 second
+            await uasyncio.sleep_ms(delay) # sleep 1 second
 
-            if not onboard_led.value():
-                print("LED off")
-            else:
-                print("LED on")
+            #if not onboard_led.value():
+            #    print("LED off")
+            #else:
+            #    print("LED on")
 
         except KeyboardInterrupt:
             break
 
 
-blync(0.25)
-print("LED starts flashing...")
-onboard_led.off()
-print("Finis!")
+async def blink_led(delay):
+    while True:
+        pc10_led.on()
+        #print("*LED ON*")
+        await uasyncio.sleep_ms(delay)
+
+        pc10_led.off()
+        #print("*LED OFF*")
+        await uasyncio.sleep_ms(delay)
+
+
+#uasyncio.run(blink_led())
+
+
+
+async def main():
+    #task = uasyncio.create_task(blync(1000))
+    #task = uasyncio.create_task(blink_led(1000))
+    task = uasyncio.gather(blink_led(250), blync(1000))
+    await task
+
+
+uasyncio.run(main())
+#blync(0.5)
+#print("LED starts flashing...")
+#onboard_led.off()
+#print("Finis!")
