ffbe1c23d151645d63e1e79c86d6c7abcfd2b9d0
Kevin James Baxter <meta.data@verizon.net>
2026-07-23 01:07:04 -0400

    Autostart.

--- flash_led/main.py
+++ flash_led/main.py
@@ -1,57 +1,17 @@
 
 
 from machine import Pin
-#from utime import sleep
-import uasyncio
+import time
 
 
-onboard_led = Pin("PA5", Pin.OUT)
-onboard_led .off()
-pc10_led = Pin("PC10", Pin.OUT)
-pc10_led.off()
-
-###############################################################################
-################################### blync LED #################################
-###############################################################################
-async def blync(delay):
-    while True:
-        try:
-            onboard_led.value(not onboard_led.value())
-            await uasyncio.sleep_ms(delay) # sleep 1 second
-
-            #if not onboard_led.value():
-            #    print("LED off")
-            #else:
-            #    print("LED on")
-
-        except KeyboardInterrupt:
-            break
+pin_47_led = Pin(47, Pin.OUT)
+pin_47_led.off()
 
 
-async def blink_led(delay):
-    while True:
-        pc10_led.on()
-        #print("*LED ON*")
-        await uasyncio.sleep_ms(delay)
-
-        pc10_led.off()
-        #print("*LED OFF*")
-        await uasyncio.sleep_ms(delay)
+while True:
+    pin_47_led.on()
+    time.sleep(0.25)
+    pin_47_led.off()
+    time.sleep(0.25)
 
 
-#uasyncio.run(blink_led())
-
-
-
-async def main():
-    #task = uasyncio.create_task(blync(1000))
-    #task = uasyncio.create_task(blink_led(1000))
-    task = uasyncio.gather(blink_led(125), blync(1000))
-    await task
-
-
-uasyncio.run(main())
-#blync(0.5)
-#print("LED starts flashing...")
-#onboard_led.off()
-#print("Finis!")
